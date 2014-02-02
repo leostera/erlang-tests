01100528d1d19e70dd284c2b623693af33b481f6
Leandro Ostera <me@leostera.com>
2014-02-02 18:48:57 -0300

    Nicer comments

--- recursion.erl
+++ recursion.erl
@@ -1,12 +1,13 @@
 -module(recursion).
 -export([len/1, tail_len/1]).
 
-% so functions can be defined by cases
-% which is pretty awesome
+% len list
+% return the length of a list applying recursion
 len([]) -> 0;
 len([_|T]) -> 1 + len(T).
 
-% tail_recursion!
+% tail_len list
+% returns the length of a list applying tail recursion
 % {little piggy}
 tail_len(L) -> tail_len(L, 0).
 tail_len([], Acc) -> Acc;

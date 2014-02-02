3d4d55abb22c8af340075fcfee3d88b6db710042
Leandro Ostera <me@leostera.com>
2014-02-02 18:51:05 -0300

    Duplicate function !

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([len/1, tail_len/1]).
+-export([len/1, tail_len/1, duplicate/2]).
 
 % len list
 % return the length of a list applying recursion
@@ -12,3 +12,10 @@ len([_|T]) -> 1 + len(T).
 tail_len(L) -> tail_len(L, 0).
 tail_len([], Acc) -> Acc;
 tail_len([_|T], Acc) -> tail_len(T, Acc+1).
+
+% duplicate
+% returns a list with N times Term
+duplicate(0, _) -> [];
+duplicate(N, Term) when N > 0 ->
+  [Term|duplicate(N-1,Term)].
+

017849a097aad4bfe6324e3b6df15ee4b3e07912
Leandro Ostera <me@leostera.com>
2014-02-02 18:45:05 -0300

    Add tail_recursion implementation for length

--- recursion.erl
+++ recursion.erl
@@ -1,8 +1,13 @@
 -module(recursion).
--export([len/1]).
+-export([len/1, tail_len/1]).
 
 % so functions can be defined by cases
 % which is pretty awesome
 len([]) -> 0;
 len([_|T]) -> 1 + len(T).
 
+% tail_recursion!
+% {little piggy}
+tail_len(L) -> tail_len(L, 0).
+tail_len([], Acc) -> Acc;
+tail_len([_|T], Acc) -> tail_len(T, Acc+1).

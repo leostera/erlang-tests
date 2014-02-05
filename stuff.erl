181503fa6caed71b66f8379eea20daa2ae19d4a1
Leandro Ostera <me@leostera.com>
2014-02-05 18:44:58 -0300

    Adds quicksort

--- stuff.erl
+++ stuff.erl
@@ -1,6 +1,6 @@
 -module(stuff).
 -export([
-        len/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, tolerant_zip/2, tail_zip/2
+        len/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, tolerant_zip/2, tail_zip/2, quicksort/1
         ]).
 
 % len list
@@ -81,3 +81,18 @@ tail_zip([], _, List) -> List;
 tail_zip(_, [], List) -> List;
 tail_zip([A|As], [B|Bs], List) ->
   tail_zip(As, Bs, List++[{A,B}]).
+
+% quicksort list
+% sorts a list using quicksort
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  {Smaller, Larger} = partition(Pivot, Rest, [], []),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+% partition pivot, rest, smaller_part, larger_part
+% returns a hash with the new smaller and larger parts
+partition(_, [], Smaller, Larger) -> {Smaller, Larger};
+partition(Pivot, [H|T], Smaller, Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+     H >  Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.

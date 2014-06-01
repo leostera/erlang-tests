0e13ec2abe100cc51f2670183a5ca3360c6f5585
Leandro Ostera <me@leostera.com>
2014-06-01 19:35:18 +0200

    shop and lists

---
+++ shop.erl
@@ -0,0 +1,12 @@
+-module(shop).
+-export([cost/1, total/1]).
+-import(lists, [map/2, sum/1]).
+
+cost(oranges)   -> 5;
+cost(newspaper) -> 8;
+cost(apples)    -> 2;
+cost(pears)     -> 4;
+cost(milk)      -> 7.
+
+total(L) -> 
+  sum(map(fun({What, N}) -> shop:cost(What)*N end, L)).

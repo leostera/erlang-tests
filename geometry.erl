caf78597fee20f31f68d4d3a8c27a02efce4407e
Leandro Ostera <me@leostera.com>
2014-06-01 17:24:51 +0200

    simple tests

--- geometry.erl
+++ geometry.erl
@@ -1,5 +1,10 @@
 -module(geometry).
--export([area/1]).
+-export([area/1, test/0]).
+
+test() ->
+  12 = area({rect, 3, 4}),
+  144 = area({square, 12}),
+  tests_green.
 
 area({rect, Width, Height}) -> Width * Height;
 area({square, Side}) -> Side * Side.

e22084591c1375fdbf1a19408405aa0f739897c6
Leandro Ostera <me@leostera.com>
2014-06-01 17:50:02 +0200

    add circle area

--- geometry.erl
+++ geometry.erl
@@ -4,8 +4,10 @@
 test() ->
   12 = area({rect, 3, 4}),
   144 = area({square, 12}),
+  9.424769999999999 = area({circle, 3}),
   tests_green.
 
 area({rect, Width, Height}) -> Width * Height;
-area({square, Side}) -> Side * Side.
+area({square, Side}) -> Side * Side;
+area({circle, Radius}) -> 3.14159 * Radius.
 
